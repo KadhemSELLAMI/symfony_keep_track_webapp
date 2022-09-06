6788ac563b49638a2f6a2c36eab8a81146376d22
SELLAMI Kadhem <thespartanathlete@gmail.com>
2022-09-06 16:24:03 +0100

    stuck in adding exercices

--- src/Controller/DayController.php
+++ src/Controller/DayController.php
@@ -6,7 +6,9 @@ use App\Repository\DayRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Day;
+use App\Entity\Exercice;
 use App\Form\DayType;
+use App\Form\ExerciceType;
 use App\Repository\ExerciceRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
@@ -56,10 +58,39 @@ class DayController extends AbstractController
             $em->persist($day);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('index'));
+            return $this->redirect($this->generateUrl('add_exe'));
         }
 
         return $this->render('create.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/add_exe", name="add_exe")
+     */
+    public function addExercices(Request $request, ManagerRegistry $doctrine)
+    {
+        $exercice = new Exercice();
+
+        $form = $this->createForm(ExerciceType::class, $exercice);
+
+        $form->handleRequest($request);
+
+        
+
+        if ($form->isSubmitted()) {
+
+            dd($exercice);
+
+            $em = $doctrine->getManager();
+            $em->persist($exercice);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('index'));
+        }
+
+        return $this->render('exercice/create.html.twig', [
             "form" => $form->createView()
         ]);
     }
--- src/Form/ExerciceType.php
+++ src/Form/ExerciceType.php
@@ -2,21 +2,22 @@
 
 namespace App\Form;
 
-use App\Entity\Day;
+use App\Entity\Exercice;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class DayType extends AbstractType
+class ExerciceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name')
-            ->add('date')
             ->add('description')
-            ->add('Exercices', SubmitType::class, [
+            ->add('sets')
+            ->add('reps')
+            ->add('Finish', SubmitType::class, [
                 'attr' => [
                     'class' => "btn btn-success float-right"
                 ]
@@ -27,7 +28,7 @@ class DayType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Day::class,
+            'data_class' => Exercice::class,
         ]);
     }
 }
